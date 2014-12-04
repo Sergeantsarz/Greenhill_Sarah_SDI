279358d0394a37b14703a81b1ce492d118420f70
Sarah Greenhill <sarah@sarahgreenhill.com>
2014-12-04 16:19:54 -0500

    Progress 1
    Completed problem 1 and 2.

--- Expressions_worksheet/js/script.js
+++ Expressions_worksheet/js/script.js
@@ -1,9 +1,24 @@
 //Sarah R. Greenhill 12/4/14 Expressions Worksheet
 
-//Problem 1 - Dogs age 7 times faster than humans so a dog that is 1 year old in human years is 7 years old in “dog years.”  Calculate how old Sparky the pit bull is in dog years based on his actual age.
+//Dog Years - Dogs age 7 times faster than humans so a dog that is 1 year old in human years is 7 years old in “dog years.”  Calculate how old Sparky the pit bull is in dog years based on his actual age.
 
-var sparkysAge = 1;
-var humanYears = 7; 
-var dogYears = sparkysAge * humanYears;
+/* var sparkysAge = 7; //Setting Sparky's accelerated dog age
+var ageInHuman = 5; //Setting what age Sparky is in human years
+var dogYears = sparkysAge * ageInHuman; //Calculating how old Sparky actually is
 
-console.log(dogYears);
+console.log("Sparky is " + ageInHuman + " human years old which is " + dogYears + " in dog years!");
+*/
+
+//Slice of Pie Part 1 - A bunch of students are having a party and somebody ordered pizza. Create an expression that calculates how much pizza each partygoer will get at the party.  (Assume all pizzas have the same number of slices and that the person dividing the pizza is really precise, so this can be a decimal, like 3.52 slices, etc.)
+
+/* var slicesPerPizza = 8; //Setting how many slices each pizza has
+var peopleAtParty = 8; //Setting how many people attend the party
+var pizzasOrdered = 2; //Setting how many pizzas they ordered
+var eachPersonGot = (slicesPerPizza * pizzasOrdered) / peopleAtParty; //Calculating how many slices each person gets
+
+console.log("Each person ate " + eachPersonGot + " slices of pizza at the party.")
+*/
+
+
+
+
