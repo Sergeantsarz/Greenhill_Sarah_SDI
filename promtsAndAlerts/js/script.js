7c50c42f02b831fabc8b6f34357d717dab152445
Sarah Greenhill <sarah@sarahgreenhill.com>
2014-12-04 15:45:50 -0500

    Prompts
    Using prompts to bring up prompt window, creating variables that store
    inputted prompt info.

---
+++ promtsAndAlerts/js/script.js
@@ -0,0 +1,11 @@
+//Using prompts and alerts with JavaScript
+
+/* prompt("Enter your year of birth:"); //Is not remembered 
+
+var userInput = prompt("Enter your year of birth:"); //Stored in the variable 
+*/
+
+var width = prompt("We are calculating the area of a rectangle. \nPlease enter the width.");
+var height = prompt("Please enter the height.");
+var area = width * height; //Calculating inputted info 
+console.log(area);
