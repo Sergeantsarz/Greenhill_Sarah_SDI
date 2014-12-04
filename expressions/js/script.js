9b240c343658f429747a647791e23d51f89129f6
Sarah Greenhill <sarah@sarahgreenhill.com>
2014-12-04 15:13:56 -0500

    Expressions - Strings/Integers
    Concatenating strings and integers.

--- expressions/js/script.js
+++ expressions/js/script.js
@@ -49,9 +49,19 @@ console.log(total);
 
 //Concatenating
 
+/*
 var firstName = "Sarah";
 var lastName = "Greenihll";
 var fullName = firstName + " " + lastName;
 
 console.log(fullName);
+*/
+
+var a = "6";
+var b = "7";
+
+var result = a + b;
+
+console.log(result);
+
 
