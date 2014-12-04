f82fcdb41521c9a443f37a522a28a6b200b8e10a
Sarah Greenhill <sarah@sarahgreenhill.com>
2014-12-04 14:23:54 -0500

    Variables And Arrays
    Learning the different kinds of variables, about strings, arrays, and
    index numbers.

--- valuesAndVariables/js/script.js
+++ valuesAndVariables/js/script.js
@@ -7,6 +7,26 @@ console.log(yearOfBirth);
 alert(yearOfBirth);
 */
 
-var age = 26; 
+/*  
 var yearOfBirth = 1988; 
+*/
+
+var age = 26; //Number Variable
+
+var isStudent = true; //Boolean Variable
+
+var firstName = "Sarah"; //Strings
+
+var stringWithQuotes = 'Ain\'t nobody got time for that!'; //Using quotes in strings
+
+var stringMultiline = "Merry Christmas ya filthy animal, \n And a happy New Year." //Separating strings on different lines
+
+var muppetNames = ["Kermit" , "Miss Piggy" , "Gonzo" , "Rizzo The Rat"]; //Arrays start at 0
+
+console.log(muppetNames[1]); //Array Access Notation - Can call out array elements by index number or by a variable equaling said index number
+
+
+
+
+
 
