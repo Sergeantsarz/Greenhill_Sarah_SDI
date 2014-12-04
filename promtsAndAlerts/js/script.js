5a5fb68d595cc2d01c02292ba9d1e86415b1dbc9
Sarah Greenhill <sarah@sarahgreenhill.com>
2014-12-04 15:50:06 -0500

    Using Alerts
    Using an alert to output the information that is input into a variable
    by the user.

--- promtsAndAlerts/js/script.js
+++ promtsAndAlerts/js/script.js
@@ -8,4 +8,6 @@ var userInput = prompt("Enter your year of birth:"); //Stored in the variable
 var width = prompt("We are calculating the area of a rectangle. \nPlease enter the width.");
 var height = prompt("Please enter the height.");
 var area = width * height; //Calculating inputted info 
-console.log(area);
+
+alert("The area of your rectangle is " + area + "!"); //Output 
+
