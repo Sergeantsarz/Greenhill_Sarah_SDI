5d58494463f1c854086318663634c624c780879a
Sarah Greenhill <sarah@sarahgreenhill.com>
2014-12-04 15:07:22 -0500

    Concatenating
    Using concatenation to output things properly.

--- expressions/js/script.js
+++ expressions/js/script.js
@@ -1,6 +1,6 @@
 //Expressions
 
-/*var a = 2; //Sets the variable
+/* var a = 2; //Sets the variable
 a = a + 3; //Adding 3 to a by including an assignment operator 
 */
 
@@ -40,8 +40,18 @@ var perimeter = length * 2 + width * 2; //Don't use uneccessary parenthesis
 
 //Expressions with Arrays
 
-var orangeBins = [234, 567, 883];
+/* var orangeBins = [234, 567, 883];
 
 var total = orangeBins[0] + orangeBins[1] + orangeBins[2];
 
 console.log(total);
+*/
+
+//Concatenating
+
+var firstName = "Sarah";
+var lastName = "Greenihll";
+var fullName = firstName + " " + lastName;
+
+console.log(fullName);
+
