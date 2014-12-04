446b932886785246042be9aa8fe92190918088f2
Sarah Greenhill <sarah@sarahgreenhill.com>
2014-12-04 16:01:14 -0500

    Expressions Worksheet Setup
    Setup files for expressions worksheet assignment in week 2.

---
+++ Expressions_worksheet/js/script.js
@@ -0,0 +1,9 @@
+//Sarah R. Greenhill 12/4/14 Expressions Worksheet
+
+//Problem 1 - Dogs age 7 times faster than humans so a dog that is 1 year old in human years is 7 years old in “dog years.”  Calculate how old Sparky the pit bull is in dog years based on his actual age.
+
+var sparkysAge = 1;
+var humanYears = 7; 
+var dogYears = sparkysAge * humanYears;
+
+console.log(dogYears);
