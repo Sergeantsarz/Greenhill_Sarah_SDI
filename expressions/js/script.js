92c2c47377980abb9ceb04b40917d05cb8eb7178
Sarah Greenhill <sarah@sarahgreenhill.com>
2014-12-04 14:59:00 -0500

    Expressions and Arrays
    Using arrays in expressions to find a total.

--- expressions/js/script.js
+++ expressions/js/script.js
@@ -27,14 +27,21 @@ console.log(remainder);
 //Order of Operations - PEMDAS 
 
 //Find the average(mean) - sum of the items divided by the number of items
-var quiz1 = 87;
+/* var quiz1 = 87;
 var quiz2 = 100;
 var quiz3 = 60;
 var quiz4 = 80;
 
 var average = (quiz1 + quiz2 + quiz3 + quiz4) / 4; 
 
-/* console.log(average);
+var perimeter = length * 2 + width * 2; //Don't use uneccessary parenthesis
+
 */
 
-var perimeter = length * 2 + width * 2; //Don't use uneccessary parenthesis
+//Expressions with Arrays
+
+var orangeBins = [234, 567, 883];
+
+var total = orangeBins[0] + orangeBins[1] + orangeBins[2];
+
+console.log(total);
