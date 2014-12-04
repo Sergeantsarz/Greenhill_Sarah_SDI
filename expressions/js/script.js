7a50062c65020f3ff54078f36d3352270d13e028
Sarah Greenhill <sarah@sarahgreenhill.com>
2014-12-04 14:38:57 -0500

    Expressions And Operators
    Using subtraction arithmetic operators in expressions.

---
+++ expressions/js/script.js
@@ -0,0 +1,12 @@
+//Expressions
+
+/*var a = 2; //Sets the variable
+a = a + 3; //Adding 3 to a by including an assignment operator 
+*/
+
+//Arithmetic Operators - + / *
+
+var yearBorn = 1988; 
+var age = 2014 - yearBorn; //Expression using subtraction operator
+
+console.log(age);
