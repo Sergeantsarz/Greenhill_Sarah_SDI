d755c2d1ff07e86076e077a47862348e1e801c28
Sarah Greenhill <sarah@sarahgreenhill.com>
2014-12-04 21:42:13 -0500

    Industry Expression Fix
    Fixed the array for each student to have unique test scores.

--- Greenhill_Sarah_ExpressionsAssignment/Expressions_Industry/js/script.js
+++ Greenhill_Sarah_ExpressionsAssignment/Expressions_Industry/js/script.js
@@ -3,10 +3,10 @@
 //Industry Problem: Sarah, Kyle, Ed, Stephanie, and Amanda are all web developers taking the same programming class. They have 8 graded assignments for this class. Upon reciving their test scores and final grade, each student can do extra credit assignments to bring their grade up. Calculate each student's final grade(average) and how many extra credit assignments they would need for a full letter grade improvement. 
 
 var sarah = [100 , 90 , 80 , 40 , 90 , 100 , 98 , 85];
-var kyle = [100 , 90 , 80 , 40 , 90 , 100 , 98 , 85];
-var ed = [100 , 90 , 80 , 40 , 90 , 100 , 98 , 85];
-var steph = [100 , 90 , 80 , 40 , 90 , 100 , 98 , 85];
-var amanda = [100 , 90 , 80 , 40 , 90 , 100 , 98 , 85];
+var kyle = [80 , 90 , 70 , 88 , 100 , 100 , 65 , 83];
+var ed = [90 , 76 , 55 , 82 , 90 , 100 , 70 , 80];
+var steph = [30 , 60 , 75 , 65 , 70 , 60 , 0 , 20];
+var amanda = [90 , 95 , 100 , 75 , 88 , 90 , 92 , 100];
 
 
 
