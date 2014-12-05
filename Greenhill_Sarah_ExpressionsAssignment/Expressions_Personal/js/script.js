c5d00755b8c30ecfaef9c5840981b6e136ecf85c
Sarah Greenhill <sarah@sarahgreenhill.com>
2014-12-04 20:26:59 -0500

    Personal Problems 2
    Adding to the Personal Js file up to the prompts and starting the
    array.

--- Greenhill_Sarah_ExpressionsAssignment/Expressions_Personal/js/script.js
+++ Greenhill_Sarah_ExpressionsAssignment/Expressions_Personal/js/script.js
@@ -1,5 +1,23 @@
 //Sarah R. Greenhill 12/4/14 Expressions Assignment PERSONAL
 
-//Personal Problem (lol): Each morning, Sarah wakes up and must take a series of medications in order to be able to function properly for the rest of the day. Calculate how much energy she's lost by the end of the week. 
+//Personal Problem (lol): Each morning, Sarah wakes up and must take a series of medications in order to be able to function properly for the rest of the day. Daily activity reduces end of day energy cache, affecting the week's resulting energy reserve. Calculate how much energy she's lost by the end of the week and how much she has left. 
 
-var totalEnergy = 500;
+var totalEnergy = 700;
+var medValue = 10;
+var medsPerDay = 10;
+var medsMissedMon = prompt("How many medications did Sarah miss on Monday? Choose 0 to 10.");
+var medsMissedTues = prompt("How many medications did Sarah miss on Tuesday? Choose 0 to 10.");
+var medsMissedWed = prompt("How many medications did Sarah miss on Wednesday? Choose 0 to 10.");
+var medsMissedThurs = prompt("How many medications did Sarah miss on Thursday? Choose 0 to 10.");
+var medsMissedFri = prompt("How many medications did Sarah miss on Friday? Choose 0 to 10.");
+var medsMissedSat = prompt("How many medications did Sarah miss on Saturday? Choose 0 to 10.");
+var medsMissedSun = prompt("How many medications did Sarah miss on Sunday? Choose 0 to 10.");
+
+var totalMissed = medMissedMon + medsMissedTues + medsMissedWed + medsMissedThurs + medsMissedFri + medsMissedSat + medsMissedSun; 
+var totalMedEnergyMissed = totalMissed * medValue; 
+
+var endWeekEnergy = totalEnergy - totalMedEnergyMissed; 
+
+
+var activityLevel [10 , 20 , 30 , 40 , 50];
+
