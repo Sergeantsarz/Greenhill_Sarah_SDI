7d3375444fdd3380c3d9e7c876d4707b8c981804
Sarah Greenhill <sarah@sarahgreenhill.com>
2014-12-04 21:52:06 -0500

    Industry Expression Arrays
    Making sure the arrays are working along with the math doing out the
    averages of all student grades.

--- Greenhill_Sarah_ExpressionsAssignment/Expressions_Industry/js/script.js
+++ Greenhill_Sarah_ExpressionsAssignment/Expressions_Industry/js/script.js
@@ -2,12 +2,19 @@
 
 //Industry Problem: Sarah, Kyle, Ed, Stephanie, and Amanda are all web developers taking the same programming class. They have 8 graded assignments for this class. Upon reciving their test scores and final grade, each student can do extra credit assignments to bring their grade up. Calculate each student's final grade(average) and how many extra credit assignments they would need for a full letter grade improvement. 
 
-var sarah = [100 , 90 , 80 , 40 , 90 , 100 , 98 , 85];
+var sarah = [100 , 90 , 85 , 99 , 90 , 100 , 98 , 85];
 var kyle = [80 , 90 , 70 , 88 , 100 , 100 , 65 , 83];
-var ed = [90 , 76 , 55 , 82 , 90 , 100 , 70 , 80];
+var ed = [90 , 76 , 55 , 82 , 90 , 100 , 55 , 80];
 var steph = [30 , 60 , 75 , 65 , 70 , 60 , 0 , 20];
-var amanda = [90 , 95 , 100 , 75 , 88 , 90 , 92 , 100];
+var amanda = [90 , 73 , 100 , 75 , 70 , 90 , 76 , 69];
 
+var avgSa = (sarah[0] + sarah[1] + sarah[2] + sarah[3] + sarah[4] + sarah[5] + sarah[6] + sarah[7]) / 8;
+var avgK = (kyle[0] + kyle[1] + kyle[2] + kyle[3] + kyle[4] + kyle[5] + kyle[6] + kyle[7]) / 8;
+var avgE = (ed[0] + ed[1] + ed[2] + ed[3] + ed[4] + ed[5] + ed[6] + ed[7]) / 8;
+var avgSt = (steph[0] + steph[1] + steph[2] + steph[3] + steph[4] + steph[5] + steph[6] + steph[7]) / 8;
+var avgA = (amanda[0] + amanda[1] + amanda[2] + amanda[3] + amanda[4] + amanda[5] + amanda[6] + amanda[7]) / 8;
+
+console.log("Sarah's average is " + avgSa + "\nKyle's average is " + avgK + "\nEd's average is " + avgE + "\nStephanie's average is " + avgSt + "\n Amanda's average is " + avgA);
 
 
 
