78bdf48a31ad743d7dd23dc35fa921e7c13eb9ef
Sarah Greenhill <sarah@sarahgreenhill.com>
2014-12-04 21:11:41 -0500

    Personal Problem Expressions Assignment
    Finishing up the problem, fixing syntax, changing variables, getting
    the console log to look and output correctly.

--- Greenhill_Sarah_ExpressionsAssignment/Expressions_Personal/js/script.js
+++ Greenhill_Sarah_ExpressionsAssignment/Expressions_Personal/js/script.js
@@ -2,8 +2,8 @@
 
 //Personal Problem (lol): Each morning, Sarah wakes up and must take a series of medications in order to be able to function properly for the rest of the day. Daily activity reduces end of day energy cache, affecting the week's resulting energy reserve. Calculate how much energy she's lost by the end of the week and how much she has left. 
 
-var totalEnergy = 700;
-var medValue = 10;
+var totalEnergy = 100;
+var medValue = 5;
 var medsPerDay = 10;
 var medsMissedMon = prompt("How many medications did Sarah miss on Monday? Choose 0 to 10.");
 var medsMissedTues = prompt("How many medications did Sarah miss on Tuesday? Choose 0 to 10.");
@@ -15,11 +15,17 @@ var medsMissedSun = prompt("How many medications did Sarah miss on Sunday? Choos
 
 var totalMissed = Number(medsMissedMon) + Number(medsMissedTues) + Number(medsMissedWed) + Number(medsMissedThurs) + Number(medsMissedFri) + Number(medsMissedSat) + Number(medsMissedSun); 
 
-
 var totalMedEnergyMissed = totalMissed * medValue; 
-
 var endWeekEnergy = totalEnergy - totalMedEnergyMissed; 
+var restDays = [10 , 20 , 30 , 40];
 
-var activityLevel = [10 , 20 , 30 , 40 , 50];
+var netEnergy0 = restDays[0] + endWeekEnergy;
+var netEnergy1 = restDays[1] + endWeekEnergy;
+var netEnergy2 = restDays[2] + endWeekEnergy;
+var netEnergy3 = restDays[3] + endWeekEnergy;
+
+
 
 console.log("This week, Sarah missed her medications on the following days in the following amounts: \nMonday: " + medsMissedMon + "\nTuesday: " + medsMissedTues + " \nWednesday: " + medsMissedWed + "\nThursday: " + medsMissedThurs + "\nFriday: " + medsMissedFri + " \nSaturday: " + medsMissedSat + "\nSunday: " + medsMissedSun);
+
+console.log("Sarah started with " + totalEnergy + "% energy at the beginning of the week, and ended up with " + endWeekEnergy + ". Taking all meds and resting for one day will leave Sarah with " + netEnergy0 + " energy. Two rest days will leave her with " + netEnergy1 + " energy. Three rest days will leave her with " + netEnergy2  + " energy. Four full rest days will leave Sarah with " + netEnergy3 + " energy.")
