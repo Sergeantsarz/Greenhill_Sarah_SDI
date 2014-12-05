b559aa22d339d3d5c8b1cd39c495ba839969d3d0
Sarah Greenhill <sarah@sarahgreenhill.com>
2014-12-04 21:38:56 -0500

    Industry Expression
    Set up the problem and the arrays for the industry expression for week
    2 assignment.

--- Greenhill_Sarah_ExpressionsAssignment/Expressions_Industry/js/script.js
+++ Greenhill_Sarah_ExpressionsAssignment/Expressions_Industry/js/script.js
@@ -1,2 +1,13 @@
 //Sarah R. Greenhill 12/4/14 Expressions Assignment INDUSTRY 
 
+//Industry Problem: Sarah, Kyle, Ed, Stephanie, and Amanda are all web developers taking the same programming class. They have 8 graded assignments for this class. Upon reciving their test scores and final grade, each student can do extra credit assignments to bring their grade up. Calculate each student's final grade(average) and how many extra credit assignments they would need for a full letter grade improvement. 
+
+var sarah = [100 , 90 , 80 , 40 , 90 , 100 , 98 , 85];
+var kyle = [100 , 90 , 80 , 40 , 90 , 100 , 98 , 85];
+var ed = [100 , 90 , 80 , 40 , 90 , 100 , 98 , 85];
+var steph = [100 , 90 , 80 , 40 , 90 , 100 , 98 , 85];
+var amanda = [100 , 90 , 80 , 40 , 90 , 100 , 98 , 85];
+
+
+
+
