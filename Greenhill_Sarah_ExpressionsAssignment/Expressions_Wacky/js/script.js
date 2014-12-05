2d96078178b9356ba8d222fa5005d1245578a6dc
Sarah Greenhill <sarah@sarahgreenhill.com>
2014-12-04 22:41:39 -0500

    Wacky Expression Comments
    Added comments to the wacky expression for week 2 assignment.

--- Greenhill_Sarah_ExpressionsAssignment/Expressions_Wacky/js/script.js
+++ Greenhill_Sarah_ExpressionsAssignment/Expressions_Wacky/js/script.js
@@ -2,24 +2,24 @@
 
 //Problem: Beebee hates cats, but loves to eat them. Calcluate how many cats Beebee can eat before she becomes full. Once Beebee is full, she still wants to eat more but can't unless she poops. Calculate how many times Beebee has to poop in order to eat more cats. How many cats can Beebee still eat? Making Beebee vomit will open up more space to fit consumed cats. 
 
-var beebeeGutCapacity = 100;
-var poop = 2;
-var catNumInput = prompt("How many cats has Beebee eaten?");
-var catWeightInput = prompt("How much does one cat weigh?");
-var timesPooped = prompt("How many times did Beebee poop?");
+var beebeeGutCapacity = 100; //The value of Beebee's full belly
+var poop = 2; //The amount that each poop grants Beebee towards empty gut space
+var catNumInput = prompt("How many cats has Beebee eaten?"); //Input for the number of cats Beebee has eaten
+var catWeightInput = prompt("How much does one cat weigh?"); //Input for how much each individual cat weighs
+var timesPooped = prompt("How many times did Beebee poop?"); //Input for the amount of times Beebee pooped
 
-var totalPoop = timesPooped * poop; 
-var totalCat = catNumInput * catWeightInput; 
-var spaceLeft = beebeeGutCapacity - totalCat + totalPoop;
-var howManyMoreCats = spaceLeft / catWeightInput;
+var totalPoop = timesPooped * poop; //Calculating the total gut space Beebee gets back from pooping
+var totalCat = catNumInput * catWeightInput; //Calculating how much gut space the amount of cats Beebee has eaten occupies
+var spaceLeft = beebeeGutCapacity - totalCat + totalPoop; //Calculating how much gut space is left after eating the specified number of cats and pooping
+var howManyMoreCats = spaceLeft / catWeightInput; //Calculating how many more cats Beebee has room to eat 
 
-var vomit = [2 , 4 , 6 , 8];
-var once = howManyMoreCats + vomit[0];
-var twice = howManyMoreCats + vomit[1]
-var thrice = howManyMoreCats + vomit[2]
-var fourTimes = howManyMoreCats + vomit[3]
+var vomit = [2 , 4 , 6 , 8]; //An array containing the amounts Beebee will gain towards gut space if she vomits
+var once = howManyMoreCats + vomit[0]; //Calculating how many more cats Beebee can eat if she pukes once
+var twice = howManyMoreCats + vomit[1] //Calculating how many more cats Beebee can eat if she pukes twice
+var thrice = howManyMoreCats + vomit[2] //Calculating how many more cats Beebee can eat if she pukes three times
+var fourTimes = howManyMoreCats + vomit[3] //Calculating how many more cats Beebee can eat if she pukes four times
 
 
-console.log("Beebee has eaten " + catNumInput + " cats and pooped " + timesPooped + " times. Her belly can now hold " + howManyMoreCats + " more cats. If Beebee vomits once, she can fit " + once + " more cats, twice and she can fit " + twice + ". If she blows chunks three times, she will have room for " + thrice + " more cats, four times will leave room for " + fourTimes + " horrible felines."); 
+console.log("Beebee has eaten " + catNumInput + " cats and pooped " + timesPooped + " times. Her belly can now hold " + howManyMoreCats + " more cats. If Beebee vomits once, she can fit " + once + " more cats, twice and she can fit " + twice + ". If she blows chunks three times, she will have room for " + thrice + " more cats, four times will leave room for " + fourTimes + " horrible felines."); //Output of how many cats Beebee has eaten, how many times she pooped, how many cats her gut can now contain and how many more cats she can eat if she vomits a certain number of times 
 
 
