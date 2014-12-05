6d66877adc3ed54bc0aeaa2ea8e9b23847a1a0e3
Sarah Greenhill <sarah@sarahgreenhill.com>
2014-12-04 23:47:34 -0500

    Industry Assignment Operators
    Adding in extra assignment operators.

--- Greenhill_Sarah_ExpressionsAssignment/Expressions_Industry/js/script.js
+++ Greenhill_Sarah_ExpressionsAssignment/Expressions_Industry/js/script.js
@@ -29,6 +29,7 @@ var finalA = extraA * extraCredit + avgA; //Calculating Amanda's final grade aft
 
 console.log("Sarah's average before extra credit is " + avgSa + "\nKyle's average before extra credit is " + avgK + "\nEd's average before extra credit is " + avgE + "\nStephanie's average  before extra credit is " + avgSt + "\nAmanda's average before extra credit is " + avgA); //Output of everyone's grades before extra credit
 
+
 console.log("Sarah completed " + extraSa + " extra credit assignments. Her final average is now " + finalS + "\nKyle completed " + extraK + " extra credit assignments. His final average is now " + finalK + "\nEd completed " + extraE + " extra credit assignments. His final average is now " + finalE + "\nStephanie completed " + extraSt + " extra credit assignments. Her final average is now " + finalSt + "\nAmanda completed " + extraA + " extra credit assignments. Her final average is now " + finalA); //Output of how many extra credit assignments each student did and what their final grade is after that extra credit is applied 
 
 
--- Greenhill_Sarah_ExpressionsAssignment/Expressions_Personal/js/script.js
+++ Greenhill_Sarah_ExpressionsAssignment/Expressions_Personal/js/script.js
@@ -15,8 +15,9 @@ var medsMissedSun = prompt("How many medications did Sarah miss on Sunday? Choos
 
 var totalMissed = Number(medsMissedMon) + Number(medsMissedTues) + Number(medsMissedWed) + Number(medsMissedThurs) + Number(medsMissedFri) + Number(medsMissedSat) + Number(medsMissedSun); //This calculates the total number of missed medications for the entire week
 
-var totalMedEnergyMissed = totalMissed * medValue; //This calculates the total amount of energy Sarah lacks at the end of the week
+var totalMedEnergyMissed = (totalMissed ++) * medValue; //This calculates the total amount of energy Sarah lacks at the end of the week where the total is incremented by one as missed energy from not taking meds in the first place
 var endWeekEnergy = totalEnergy - totalMedEnergyMissed; //This calculates how much energy Sarah has remaining at the end of the week
+var endWeekFinalEnergy = endWeekEnergy -= 10; //This accounts for weekly activity which drains reserve energy
 var restDays = [10 , 20 , 30 , 40]; //This array contains values pertaining to how much energy is restored to Sarah upon taking all meds and resting
 
 var netEnergy0 = restDays[0] + endWeekEnergy; //Calculating for one day of rest/full meds
@@ -28,4 +29,4 @@ var netEnergy3 = restDays[3] + endWeekEnergy; //Calculating for four days of res
 
 console.log("This week, Sarah missed her medications on the following days in the following amounts: \nMonday: " + medsMissedMon + "\nTuesday: " + medsMissedTues + " \nWednesday: " + medsMissedWed + "\nThursday: " + medsMissedThurs + "\nFriday: " + medsMissedFri + " \nSaturday: " + medsMissedSat + "\nSunday: " + medsMissedSun + "\nThat is a total of " + totalMissed + " missed medications for the week."); //Output of how many meds Sarah missed this week on each day and in total
 
-console.log("Sarah started with " + totalEnergy + "% energy at the beginning of the week, and ended up with " + endWeekEnergy + ". Taking all meds and resting for one day will leave Sarah with " + netEnergy0 + " energy. Two rest days will leave her with " + netEnergy1 + " energy. Three rest days will leave her with " + netEnergy2  + " energy. Four full rest days will leave Sarah with " + netEnergy3 + " energy.") //Output of Sarah's total energy to start, at the end of the week, and what it will be with different amounts of rest/full meds
+console.log("Sarah started with " + totalEnergy + "% energy at the beginning of the week, and ended up with " + endWeekFinalEnergy + ". Taking all meds and resting for one day will leave Sarah with " + netEnergy0 + " energy. Two rest days will leave her with " + netEnergy1 + " energy. Three rest days will leave her with " + netEnergy2  + " energy. Four full rest days will leave Sarah with " + netEnergy3 + " energy.") //Output of Sarah's total energy to start, at the end of the week, and what it will be with different amounts of rest/full meds
