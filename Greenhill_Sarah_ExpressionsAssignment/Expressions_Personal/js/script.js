8745acf5f8cc853b97b78a3fd683dd9c54ee6147
Sarah Greenhill <sarah@sarahgreenhill.com>
2014-12-04 20:02:50 -0500

    Problem 2: Personal
    Setup and get going on the personal JS file.

--- Greenhill_Sarah_ExpressionsAssignment/Expressions_Personal/js/script.js
+++ Greenhill_Sarah_ExpressionsAssignment/Expressions_Personal/js/script.js
@@ -1,1 +1,5 @@
-//Sarah R. Greenhill 12/4/14 Expressions Assignment
+//Sarah R. Greenhill 12/4/14 Expressions Assignment PERSONAL
+
+//Personal Problem (lol): Each morning, Sarah wakes up and must take a series of medications in order to be able to function properly for the rest of the day. Calculate how much energy she's lost by the end of the week. 
+
+var totalEnergy = 500;
