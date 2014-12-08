5d0957354212bcbb83f69dac0db14f9aa72b6cb6
Sarah Greenhill <sarah@sarahgreenhill.com>
2014-12-08 18:26:52 -0500

    Using &&
    Using the && logical operator

--- conditionals/js/script.js
+++ conditionals/js/script.js
@@ -1,17 +1,14 @@
-//Conditional Logic - With an Expression If, Else, & Else If 
-var kidHeight = 30;
-var minHeight = 48; 
-var wParentHeight = 45; //height of the child with a parent 
+//Conditional Logic - Logical Operators 
+//Logical operators only go between booleans or comparisons
+// && = And || = or ^ = Exclusive or XOR ! = Not 
 
-//If the child is old enough, print to the consle "You can ride!" 
-//If the child is over 48 inches in height
-if(kidHeight > minHeight){
-	//code performed if condition is true
-	console.log("You can ride the coaster!");
-} else if (kidHeight > wParentHeight){
-	//Can ride with a parent
-	console.log("You can ride, but only with a parent present.");
-} else{
-	//If all is false, child cannot ride 
-	console.log("Sorry kid, you've got some growing to do.");
+var budget = 300;
+var iPhonePrice  = 199.99;
+var paycheck = 200;
+
+//if the price f the phone is less than budget and if our paycheck is over 300
+if(iPhonePrice < budget && paycheck > 300){
+	console.log("We can buy the phone!");
+} else {
+	consolee.log("No phone for you!");
 }
