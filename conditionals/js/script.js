c084742684757df15f57069d7b186b086ce7bc0e
Sarah Greenhill <sarah@sarahgreenhill.com>
2014-12-08 15:01:17 -0500

    Using conditionals with expressions
    Conditional statements can also include expressions within themselves.

--- conditionals/js/script.js
+++ conditionals/js/script.js
@@ -1,11 +1,12 @@
-//Conditional Logic - Relational Expressions 
+//Conditional Logic - With an Expression
 
 var kidHeight = 30;
 var minHeight = 48; 
+var sneakerLift = 2;
 
 //If the child is old enough, print to the consle "You can ride!" 
 //If the child is over 48 inches in height
-if(kidHeight > minHeight){
+if(kidHeight + snearkerLift > minHeight){
 	//code performed if condition is true
 	console.log("You can ride the coaster!");
 }
