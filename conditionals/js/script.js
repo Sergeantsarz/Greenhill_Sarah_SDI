7b363cfa1f289c35901d52618beb3ec9bc72cf50
Sarah Greenhill <sarah@sarahgreenhill.com>
2014-12-08 18:39:27 -0500

    Using ! logical operator
    Using ! or “not” operator to negate the true or false of something.

--- conditionals/js/script.js
+++ conditionals/js/script.js
@@ -7,7 +7,9 @@ var iPhonePrice  = 199.99;
 var wonLottery = true; 
 
 //if the price f the phone is less than budget and if our paycheck is over 300
-//|| only needs one in the pair to be true don't even have to write the ===
+//|| only needs one in the pair to be true don't even have to write the === 
+//^ means only one can be true, not both 
+//! uses negation to convert to fals or true 
 
 if(iPhonePrice < budget || wonLottery){
 	console.log("We can buy the phone!");
