af70d5673c6bc8ecf48891cd4969301e2cf37d38
Sarah Greenhill <sarah@sarahgreenhill.com>
2014-12-08 18:47:05 -0500

    Using ternaries
    Using the ? to write out a conditional all in one line and return
    values.

--- conditionals/js/script.js
+++ conditionals/js/script.js
@@ -1,6 +1,7 @@
 //Conditional Logic - Logical Operators 
 //Logical operators only go between booleans or comparisons
 // && = And || = or ^ = Exclusive or XOR ! = Not === = strict equality 
+//Ternaries use ? to do if else all in one line 
 
 var budget = 100;
 var iPhonePrice  = 199.99;
@@ -9,10 +10,16 @@ var wonLottery = true;
 //if the price f the phone is less than budget and if our paycheck is over 300
 //|| only needs one in the pair to be true don't even have to write the === 
 //^ means only one can be true, not both 
-//! uses negation to convert to fals or true 
+//! uses negation to convert to false or true 
 
-if(iPhonePrice < budget || wonLottery){
-	console.log("We can buy the phone!");
+/* var gps = 48;
+
+
+if (gpa > 2.0){
+	console.log("You can graduate!");
 } else {
-	consolee.log("No phone for you!");
-}
+	console.log("GPA is too low!");
+}*/
+
+//if the gpa is over the min 2.0 score, the student can graduate 
+(gpa > 2.0) ? console.log("You can graduate") : console.log("GPA too low!");
