c59083d426266d38686f8f15a0d0669e99c12973
Sarah Greenhill <sarah@sarahgreenhill.com>
2014-12-08 18:51:17 -0500

    Putting ternaries to practice
    Using a ternary to output which book a child is to receive based upon
    their age.

--- conditionals/js/script.js
+++ conditionals/js/script.js
@@ -22,4 +22,17 @@ if (gpa > 2.0){
 }*/
 
 //if the gpa is over the min 2.0 score, the student can graduate 
-(gpa > 2.0) ? console.log("You can graduate") : console.log("GPA too low!");
+/* (gpa > 2.0) ? console.log("You can graduate") : console.log("GPA too low!"); */
+
+/* var age = 11;
+var book;
+
+if(age < 10 ){
+	book = "Green Eggs And Ham";
+} else {
+	book = "The Time Machine";
+}
+console.log(book); */
+
+//If the child is under 10, they get Green Eggs And Ham, otherwise they get The Time Machine
+book = (age < 10) ? "Green Eggs And Ham" : "The Time Machine";
