9fe0ca245ce2d957a0abe33ceb2d93baa9d44012
Sarah Greenhill <sarah@sarahgreenhill.com>
2014-12-08 18:34:38 -0500

    Using || logic operator
    Using || to determine if an if statement will resolve true or false
    using the || logic operator or the “or” operator.

--- conditionals/js/script.js
+++ conditionals/js/script.js
@@ -1,13 +1,15 @@
 //Conditional Logic - Logical Operators 
 //Logical operators only go between booleans or comparisons
-// && = And || = or ^ = Exclusive or XOR ! = Not 
+// && = And || = or ^ = Exclusive or XOR ! = Not === = strict equality 
 
-var budget = 300;
+var budget = 100;
 var iPhonePrice  = 199.99;
-var paycheck = 200;
+var wonLottery = true; 
 
 //if the price f the phone is less than budget and if our paycheck is over 300
-if(iPhonePrice < budget && paycheck > 300){
+//|| only needs one in the pair to be true don't even have to write the ===
+
+if(iPhonePrice < budget || wonLottery){
 	console.log("We can buy the phone!");
 } else {
 	consolee.log("No phone for you!");
