66c11b92baa72938e260c42c87fea33a4945abf5
Sarah Greenhill <sarah@sarahgreenhill.com>
2014-12-08 15:14:24 -0500

    If, Else, & Else if
    Distinction between an if statement, an else if statement, and an else
    statement. The code progresses from if, to if else, finally ending with
    else.

--- conditionals/js/script.js
+++ conditionals/js/script.js
@@ -1,14 +1,17 @@
-//Conditional Logic - With an Expression If & Else 
-
+//Conditional Logic - With an Expression If, Else, & Else If 
 var kidHeight = 30;
 var minHeight = 48; 
-
+var wParentHeight = 45; //height of the child with a parent 
 
 //If the child is old enough, print to the consle "You can ride!" 
 //If the child is over 48 inches in height
 if(kidHeight > minHeight){
 	//code performed if condition is true
 	console.log("You can ride the coaster!");
+} else if (kidHeight > wParentHeight){
+	//Can ride with a parent
+	console.log("You can ride, but only with a parent present.");
 } else{
+	//If all is false, child cannot ride 
 	console.log("Sorry kid, you've got some growing to do.");
 }
