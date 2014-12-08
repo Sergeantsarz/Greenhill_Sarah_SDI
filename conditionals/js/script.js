2f9ab06f9daabf1266664f55b9dcaec8b6c48f48
Sarah Greenhill <sarah@sarahgreenhill.com>
2014-12-08 14:57:50 -0500

    Relational Expressions
    Using conditionals to judge the relationship between things, in this
    case, variables.

--- conditionals/js/script.js
+++ conditionals/js/script.js
@@ -1,10 +1,11 @@
-//Conditional Logic 
+//Conditional Logic - Relational Expressions 
 
-var oldEnough = false; 
+var kidHeight = 30;
+var minHeight = 48; 
 
 //If the child is old enough, print to the consle "You can ride!" 
-
-if(oldEnough){
+//If the child is over 48 inches in height
+if(kidHeight > minHeight){
 	//code performed if condition is true
 	console.log("You can ride the coaster!");
 }
