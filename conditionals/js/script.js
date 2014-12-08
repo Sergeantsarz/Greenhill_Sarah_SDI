b0dfa679f3ca82a5d3a1d1006342887b17294dd9
Sarah Greenhill <sarah@sarahgreenhill.com>
2014-12-08 14:53:42 -0500

    Learning Conditional Statements
    File setup for the conditionals section in week 3.

---
+++ conditionals/js/script.js
@@ -0,0 +1,10 @@
+//Conditional Logic 
+
+var oldEnough = false; 
+
+//If the child is old enough, print to the consle "You can ride!" 
+
+if(oldEnough){
+	//code performed if condition is true
+	console.log("You can ride the coaster!");
+}
