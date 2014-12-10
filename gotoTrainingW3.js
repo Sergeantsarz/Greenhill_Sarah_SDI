fc3ba44a7ccfc1f4862d2e2b1eabcd9214d7f490
Sarah Greenhill <sarah@sarahgreenhill.com>
2014-12-10 17:22:03 -0500

    Setting up the fruit array
    Created an array of fruits

--- gotoTrainingW3.js
+++ gotoTrainingW3.js
@@ -1,2 +1,12 @@
 //GoTo Training Week 3 
 
+//Create code that sorts through an array of fruit names and picks our pears
+
+//Creat our "fruit bowl" array
+var bowlOfFruit = ["apple" , "banana" , "pear" , "peach"];
+
+//This shows the whole array
+console.log(bowlOfFruit);
+
+//Display only one of the fruits 
+console.log(bowlOfFruit[1]);
