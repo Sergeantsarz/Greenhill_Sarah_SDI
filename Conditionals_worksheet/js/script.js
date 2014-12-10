a0bdeddff610ebd1cdc9a5f6047ea8a83104453a
Sarah Greenhill <sarah@sarahgreenhill.com>
2014-12-10 16:28:41 -0500

    First Group
    Setting up the temp converter.

--- Conditionals_worksheet/js/script.js
+++ Conditionals_worksheet/js/script.js
@@ -1,1 +1,10 @@
 //Sarah R. Greenhill - December 10, 2014 - Conditionals Worksheet
+
+//Group 1 - Celsius to Fahrenheit converter - Convert a temperature to either degrees Celsius or degrees Fahrenheit depending on what the user has entered. If the user puts a “C” for the unit, the calculator should convert to Celsius. If the user puts “F” for the unit, the calculator should convert to Fahrenheit.
+
+//F to C = Deduct 32, then multiply by 5, then divide by 9
+//C to F = Multiply by 9, then divide by 5, then add 32
+
+var celcius = "C";
+var fahrenheit = "F"; 
+
