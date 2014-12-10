5cb56fa049c520e99adfb4565397aac1391d26c5
Sarah Greenhill <sarah@sarahgreenhill.com>
2014-12-10 17:33:50 -0500

    Tested First Fruit
    Tested what the first fruit in the bowl array is.

--- gotoTrainingW3.js
+++ gotoTrainingW3.js
@@ -10,3 +10,27 @@ console.log(bowlOfFruit);
 
 //Display only one of the fruits 
 console.log(bowlOfFruit[1]);
+
+//How many items are in the array 
+//Using Dot Syntax - Using a period
+console.log(bowlOfFruit.length);
+
+//What is the length  of the array, using the length property
+//Last Index number is always one less than the length 
+// bowlOfFruit.length - 1 = length of the array 
+
+//Create a variable to track the number of pears we have
+var totalNumPears = 0; 
+
+//Test each item to see if it is a pear 
+//if (condition) {code to run if true};
+
+if(bowlOfFruit [0] === "pear"){
+	//this will run if it is true 
+	//if it's true, add 1 to the counting variable
+	console.log("This fruit is a pear!");
+	totalNumPears++;
+} else {
+	//This code will run if the IF statement is false
+	console.log("This fruit is NOT a pear!");
+}
