6f38714755effad6367574bec6f90809be76d928
Sarah Greenhill <sarah@sarahgreenhill.com>
2014-12-10 20:18:36 -0500

    Finished Group 3
    Finished and validated that all conditionals work.

--- Conditionals_worksheet/js/script.js
+++ Conditionals_worksheet/js/script.js
@@ -5,7 +5,7 @@
 //F to C = Deduct 32, then multiply by 5, then divide by 9
 //C to F = Multiply by 9, then divide by 5, then add 32
 
-/* var userInputNum = prompt ("How many degrees are we converting?"); //User is prompted for a number of degrees
+ /* var userInputNum = prompt ("How many degrees are we converting?"); //User is prompted for a number of degrees
 var userInputLet = prompt ("Choose C for Celcius or F for Fahrenheit."); //User is prompted to choose C or F
 var cToF = userInputNum * 9 / 5 + 32; //Does the math to convert to Fahrenheit
 var fToC = (userInputNum - 32) * 5 / 9; //Does the math to convert to Celcius
@@ -22,7 +22,7 @@ if(upperCaseUserLet === "C"){
 
 //Group 2 - Check the Login - Make sure the user has the correct username and password. If the username doesn’t match then a specific message for that should be printed to the console. If the password doesn’t match a message should appear for that, etc. Only one error message should appear. 
 
-var usernameInput = prompt ("Please enter your username."); //Prompts the user to input their username
+/* var usernameInput = prompt ("Please enter your username."); //Prompts the user to input their username
 var passwordInput = prompt ("Please enter your password. Reminder: This is case sensitive!"); //Prompts the user to inpit their password
 
 var username = "Sergeantsarz"; //The stored value of the proper user name
@@ -34,4 +34,22 @@ if (usernameInput === "Sergeantsarz" && passwordInput === "passWord1!"){
 } else {
 	
 	console.log("User or password not found. Please try again!");
-} //This conditional tests whether the input of the user name and password matches the stored variables. If not, the else statement returns the error message that either the password or the username is incorrect. 
+} //This conditional tests whether the input of the user name and password matches the stored variables. If not, the else statement returns the error message that either the password or the username is incorrect. */
+
+//Group 3 - Movie Ticket Price - The local movie theater in town has a ticket price of $12.00 but if you are a senior (55 and older), under 10 you get the discounted price of $7.00. In addition, if a customer is seeing a movie between 3pm and 5pm they can also get the discounted price. Determine which of the two prices the customer is eligible for. 
+
+var customerAge = prompt ("What is your age?");
+var movieTime = prompt ("What time is the movie?");
+var ticketPrice = "$12";
+
+if (customerAge >= 50 || customerAge <= 10){
+	
+	console.log("Your age (" + customerAge + ") meets the discount requirements! Your ticket will be 7 dollars for the movie at " + movieTime + ".");
+} else if (movieTime >= 3 && movieTime <= 5){
+	
+	console.log("Your movie time (" + movieTime + "PM) meets the discount requirements! Your ticket will be 7 dollars.");
+} else{
+	
+	console.log("Your ticket price is " + ticketPrice + " for the movie at " + movieTime + ".");
+}
+
