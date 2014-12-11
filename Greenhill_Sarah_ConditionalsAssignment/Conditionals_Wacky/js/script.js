7b12af342f47dfa63a5f4072795001f6b3ab030c
Sarah Greenhill <sarah@sarahgreenhill.com>
2014-12-11 16:32:32 -0500

    Testing Wacky Code
    Figuring out the wacky scenario, testing the code, making sure it
    works.

--- Greenhill_Sarah_ConditionalsAssignment/Conditionals_Wacky/js/script.js
+++ Greenhill_Sarah_ConditionalsAssignment/Conditionals_Wacky/js/script.js
@@ -2,8 +2,34 @@
 
 //Oh no, a zombie apocalypse! Who's infected and who isn't? 
 
+var survivorTemps = prompt ("It's the zombie apocalypse! Any and all survivors must be checked for infection.\nWhat is your current tempurature?");
+
+var survivorPick = prompt ("Choose an apocalypse survivor with a number from 0 to 7.");
+//var survivorTemps = ["109" , "100" , "104" , "101" , "105" , "99.9" , "35" , "98.6" , "100.3" , "103" , "102" , "97.9" , "100.2" , "10"];
 var survivors = ["Sarah" , "Paul" , "Kyle" , "Steph" , "Amanda" , "Ed" , "Tim" , "Gabby"];
 
-var lotto = prompt ("It's the zombie apocalypse and the survivors might be infected! \nChoose your lotto number from 0 - 13.");
 
-tempurature = (temp > 100) ? "They're infected!" : "They're alright!";
+if (survivorPick == 0){
+	
+		console.log("You chose survivor " + survivors[0] + ".");
+}
+
+if (survivorPick == 1){
+	
+		console.log("You chose survivor " + survivors[1] + ".");
+}
+
+
+
+if (survivorTemps >= 102 ){
+	
+	console.log("This survivor is infected, put them down or run for your life!");
+} else if (survivorTemps < 79){
+	
+	console.log("This is not a survivor at all. This person is dead.");
+}else {
+	
+	console.log("This survivor is NOT infected. They're safe, for now...");
+}
+
+
