62def4d3336e5dcb89e99fc0049094a5be50fadd
Sarah Greenhill <sarah@sarahgreenhill.com>
2014-12-11 21:40:13 -0500

    Added more students Industry
    Added more students to the array.

--- Greenhill_Sarah_ConditionalsAssignment/Conditionals_Industry/js/script.js
+++ Greenhill_Sarah_ConditionalsAssignment/Conditionals_Industry/js/script.js
@@ -6,7 +6,7 @@
 var passed = 0;
 var failed = 0;
 
-var studentList = ["Sarah" , "Bob" , "Carl" , "Daryl" , "Rick" , "Carole" , "Khaleesi" , "Hodor"];
+var studentList = ["Sarah" , "Bob" , "Carl" , "Daryl" , "Rick" , "Carole" , "Khaleesi" , "Hodor" , "Bethany" , "Charles" , "Beebee" , "Jessica" , "Garen" , "Onyx" , "Dean" , "Sam"];
 
 var student = prompt ("Did " + studentList[0] + " graduate?");
 var student2 = prompt ("Did " + studentList[1] + " graduate?");
@@ -16,6 +16,14 @@ var student5 = prompt ("Did " + studentList[4] + " graduate?");
 var student6 = prompt ("Did " + studentList[5] + " graduate?");
 var student7 = prompt ("Did " + studentList[6] + " graduate?");
 var student8 = prompt ("Did " + studentList[7] + " graduate?");
+var student9 = prompt ("Did " + studentList[8] + " graduate?");
+var student10 = prompt ("Did " + studentList[9] + " graduate?");
+var student11 = prompt ("Did " + studentList[10] + " graduate?");
+var student12 = prompt ("Did " + studentList[11] + " graduate?");
+var student13 = prompt ("Did " + studentList[12] + " graduate?");
+var student14 = prompt ("Did " + studentList[13] + " graduate?");
+var student15 = prompt ("Did " + studentList[14] + " graduate?");
+var student16 = prompt ("Did " + studentList[15] + " graduate?");
 
 
 var studentStatus = student.toUpperCase(); 
@@ -26,6 +34,15 @@ var studentStatus5 = student5.toUpperCase();
 var studentStatus6 = student6.toUpperCase();
 var studentStatus7 = student7.toUpperCase();
 var studentStatus8 = student8.toUpperCase();
+var studentStatus9 = student9.toUpperCase();
+var studentStatus10 = student10.toUpperCase();
+var studentStatus11 = student11.toUpperCase();
+var studentStatus12 = student12.toUpperCase();
+var studentStatus13 = student13.toUpperCase();
+var studentStatus14 = student14.toUpperCase();
+var studentStatus15 = student15.toUpperCase();
+var studentStatus16 = student16.toUpperCase();
+
 
 (studentStatus === "YES") ? passed++ : failed++; 
 (studentStatus2 === "YES") ? passed++ : failed++; 
@@ -35,6 +52,14 @@ var studentStatus8 = student8.toUpperCase();
 (studentStatus6 === "YES") ? passed++ : failed++; 
 (studentStatus7 === "YES") ? passed++ : failed++;
 (studentStatus8 === "YES") ? passed++ : failed++;  
+(studentStatus9 === "YES") ? passed++ : failed++; 
+(studentStatus10 === "YES") ? passed++ : failed++; 
+(studentStatus11 === "YES") ? passed++ : failed++; 
+(studentStatus12 === "YES") ? passed++ : failed++; 
+(studentStatus13 === "YES") ? passed++ : failed++; 
+(studentStatus14 === "YES") ? passed++ : failed++; 
+(studentStatus15 === "YES") ? passed++ : failed++; 
+(studentStatus16 === "YES") ? passed++ : failed++; 
 
 var averageSuccess = passed / studentList.length * 100;
 
@@ -42,4 +67,4 @@ console.log(passed + " students have graduated from Full Sail University in the 
 
 console.log(failed + " students have failed out of Full Sail University in the Web Design & Development degree program this month.");
 
-
+console.log("The average success rate of this program for this month is " + parseInt(averageSuccess) + "%.");
