0e7549ecbb9124fd554ae05ad50e49ffa5a48b91
Sarah Greenhill <sarah@sarahgreenhill.com>
2014-12-11 22:51:59 -0500

    Finished Industry Conditional Assignment
    Finished the Industry conditional assignment with validation and
    comments.

--- Greenhill_Sarah_ConditionalsAssignment/Conditionals_Industry/js/script.js
+++ Greenhill_Sarah_ConditionalsAssignment/Conditionals_Industry/js/script.js
@@ -25,23 +25,118 @@ var student15 = prompt ("Did " + studentList[14] + " graduate?"); //Prompts the 
 var student16 = prompt ("Did " + studentList[15] + " graduate?"); //Prompts the user for whether student #16 has passed
 
 
-var studentStatus = student.toUpperCase(); //Forces the user input to all caps
-var studentStatus2 = student2.toUpperCase(); //Forces the user input to all caps
-var studentStatus3 = student3.toUpperCase(); //Forces the user input to all caps
-var studentStatus4 = student4.toUpperCase(); //Forces the user input to all caps
-var studentStatus5 = student5.toUpperCase(); //Forces the user input to all caps
-var studentStatus6 = student6.toUpperCase(); //Forces the user input to all caps
-var studentStatus7 = student7.toUpperCase(); //Forces the user input to all caps
-var studentStatus8 = student8.toUpperCase(); //Forces the user input to all caps
-var studentStatus9 = student9.toUpperCase(); //Forces the user input to all caps
-var studentStatus10 = student10.toUpperCase(); //Forces the user input to all caps
-var studentStatus11 = student11.toUpperCase(); //Forces the user input to all caps
-var studentStatus12 = student12.toUpperCase(); //Forces the user input to all caps
-var studentStatus13 = student13.toUpperCase(); //Forces the user input to all caps
-var studentStatus14 = student14.toUpperCase(); //Forces the user input to all caps
-var studentStatus15 = student15.toUpperCase(); //Forces the user input to all caps
-var studentStatus16 = student16.toUpperCase(); //Forces the user input to all caps
+var studentStatus = student.toUpperCase(); //Forces the user's input to be upper case
+var studentStatus2 = student2.toUpperCase(); //Forces the user's input to be upper case
+var studentStatus3 = student3.toUpperCase(); //Forces the user's input to be upper case
+var studentStatus4 = student4.toUpperCase(); //Forces the user's input to be upper case
+var studentStatus5 = student5.toUpperCase(); //Forces the user's input to be upper case
+var studentStatus6 = student6.toUpperCase(); //Forces the user's input to be upper case
+var studentStatus7 = student7.toUpperCase(); //Forces the user's input to be upper case
+var studentStatus8 = student8.toUpperCase(); //Forces the user's input to be upper case
+var studentStatus9 = student9.toUpperCase(); //Forces the user's input to be upper case
+var studentStatus10 = student10.toUpperCase(); //Forces the user's input to be upper case
+var studentStatus11 = student11.toUpperCase(); //Forces the user's input to be upper case
+var studentStatus12 = student12.toUpperCase(); //Forces the user's input to be upper case
+var studentStatus13 = student13.toUpperCase(); //Forces the user's input to be upper case
+var studentStatus14 = student14.toUpperCase(); //Forces the user's input to be upper case
+var studentStatus15 = student15.toUpperCase(); //Forces the user's input to be upper case
+var studentStatus16 = student16.toUpperCase(); //Forces the user's input to be upper case
 
+if (student === ""){
+	
+	student = prompt ("Make sure you enter whether or not " + studentList[0] + " passed!");
+	
+} //This conditional checks to see if the user has entered a value for if the student passed 
+
+if (student2 === ""){
+	
+	student = prompt ("Make sure you enter whether or not " + studentList[1] + " passed!");
+	
+}  //This conditional checks to see if the user has entered a value for if the student passed 
+
+if (student3 === ""){
+	
+	student = prompt ("Make sure you enter whether or not " + studentList[2] + " passed!");
+		
+} //This conditional checks to see if the user has entered a value for if the student passed 
+
+if (student4 === ""){
+	
+	student = prompt ("Make sure you enter whether or not " + studentList[3] + " passed!");
+	
+} //This conditional checks to see if the user has entered a value for if the student passed 
+
+if (student5 === ""){
+	
+	student = prompt ("Make sure you enter whether or not " + studentList[4] + " passed!");
+	
+} //This conditional checks to see if the user has entered a value for if the student passed 
+
+if (student6 === ""){
+	
+	student6 = prompt ("Make sure you enter whether or not " + studentList[5] + " passed!");
+	
+} //This conditional checks to see if the user has entered a value for if the student passed 
+
+if (student7 === ""){
+	
+	student7 = prompt ("Make sure you enter whether or not " + studentList[6] + " passed!");
+	
+} //This conditional checks to see if the user has entered a value for if the student passed 
+
+if (student8 === ""){
+	
+	student8 = prompt ("Make sure you enter whether or not " + studentList[7] + " passed!");
+	
+} //This conditional checks to see if the user has entered a value for if the student passed 
+
+if (student9 === ""){
+	
+	student9 = prompt ("Make sure you enter whether or not " + studentList[8] + " passed!");
+	
+} //This conditional checks to see if the user has entered a value for if the student passed 
+
+if (student10 === ""){
+	
+	student10 = prompt ("Make sure you enter whether or not " + studentList[9] + " passed!");
+	
+} //This conditional checks to see if the user has entered a value for if the student passed 
+
+if (student11 === ""){
+	
+	student11 = prompt ("Make sure you enter whether or not " + studentList[10] + " passed!");
+	
+} //This conditional checks to see if the user has entered a value for if the student passed 
+
+if (student12 === ""){
+	
+	student12 = prompt ("Make sure you enter whether or not " + studentList[11] + " passed!");
+	
+} //This conditional checks to see if the user has entered a value for if the student passed 
+
+if (student13 === ""){
+	
+	student13 = prompt ("Make sure you enter whether or not " + studentList[12] + " passed!");
+	
+} //This conditional checks to see if the user has entered a value for if the student passed 
+
+if (student14 === ""){
+	
+	student14 = prompt ("Make sure you enter whether or not " + studentList[13] + " passed!");
+	
+} //This conditional checks to see if the user has entered a value for if the student passed 
+
+if (student15 === ""){
+	
+	student15 = prompt ("Make sure you enter whether or not " + studentList[14] + " passed!");
+	
+} //This conditional checks to see if the user has entered a value for if the student passed 
+
+if (student16 === ""){
+	
+	student16 = prompt ("Make sure you enter whether or not " + studentList[15] + " passed!");
+	
+} //This conditional checks to see if the user has entered a value for if the student passed 
 
 (studentStatus === "YES") ? passed++ : failed++; //Adds to passed or failed container based off of if the studentStatus variable is true or false
 (studentStatus2 === "YES") ? passed++ : failed++; //Adds to passed or failed container based off of if the studentStatus2 variable is true or false
@@ -62,8 +157,20 @@ var studentStatus16 = student16.toUpperCase(); //Forces the user input to all ca
 
 var averageSuccess = passed / studentList.length * 100; //Gets the average success rate by dividing the number of students that passed by the amount of students in teh array and multiplying that by 100
 
+
 console.log(passed + " students have graduated from Full Sail University in the Web Design & Development degree program this month."); //Tells the user how many students have passed
 
 console.log(failed + " students have failed out of Full Sail University in the Web Design & Development degree program this month."); //Tells the user how many students have failed
 
 console.log("The average success rate of this program for this month is " + parseInt(averageSuccess) + "%."); //Tells the user the average rate of success for this program in this particular month
+
+if(averageSuccess > 50 && averageSuccess < 100 ){
+	
+	console.log("The success rate for this month is more than half! It is a glass half full.");
+} //This conditional tests if the success percentage is more than half and lets the user know
+
+if(averageSuccess < 50 && averageSuccess < 100 ){
+	
+	console.log("The success rate for this month is less than half! It is a glass less than half empty.");
+} //This conditional tests if the success percentage is less than half and lets the user know
+
