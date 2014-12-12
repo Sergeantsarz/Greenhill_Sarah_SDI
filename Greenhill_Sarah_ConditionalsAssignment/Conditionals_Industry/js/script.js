7fe930f5be60066af8d13a0ac18c9d7f13efb078
Sarah Greenhill <sarah@sarahgreenhill.com>
2014-12-11 21:14:05 -0500

    Setting Up Industry Conditonals Assignment
    Adding things to the document, setting up the math and such.

--- Greenhill_Sarah_ConditionalsAssignment/Conditionals_Industry/js/script.js
+++ Greenhill_Sarah_ConditionalsAssignment/Conditionals_Industry/js/script.js
@@ -1,3 +1,24 @@
 //Sarah R. Greenhill - 12/11/14 - Conditionals Assignment - Industry
 
-//Full Sail Student drop-out rate 
+//Full Sail University isn't only known as a great place to go to school, but also for how difficult it is to be successful there if you are not diligent enough. Calculate Full Sail Student drop-out rates and let them know the probability of getting a job upon graduation from the BS Web Design & Dev degree program. 
+
+// The program is designed to take 27 months to complete. Of those that completed the program in 2012-2013, 55% finished in 27 months. The job placement rate for students who completed this program is 75%.
+var passed = 0;
+var failed = 0;
+
+var studentList ["Sarah" , "Bob" , "Carl" , "Daryl" , "Rick" , "Carole" , "Khaleesi" , "Hodor"];
+var student = prompt ("Did this student graduate?");
+var student2 = prompt ("Did this student graduate?");
+var student3 = prompt ("Did this student graduate?");
+var student4 = prompt ("Did this student graduate?");
+var student5 = prompt ("Did this student graduate?");
+var student6 = prompt ("Did this student graduate?");
+var student7 = prompt ("Did this student graduate?");
+var student8 = prompt ("Did this student graduate?");
+var student9 = prompt ("Did this student graduate?");
+var student10 = prompt ("Did this student graduate?");
+
+var studentStatus = student.toUpperCase(); 
+
+(studentStatus === "YES") ? passed++ : failed++; 
+
