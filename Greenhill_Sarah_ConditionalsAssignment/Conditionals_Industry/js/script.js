948e15b4444f37742fc213203a13e8c527d840f6
Sarah Greenhill <sarah@sarahgreenhill.com>
2014-12-11 21:42:47 -0500

    Adding Comments Industry
    Started Adding Comments.

--- Greenhill_Sarah_ConditionalsAssignment/Conditionals_Industry/js/script.js
+++ Greenhill_Sarah_ConditionalsAssignment/Conditionals_Industry/js/script.js
@@ -2,28 +2,27 @@
 
 //Full Sail University isn't only known as a great place to go to school, but also for how difficult it is to be successful there if you are not diligent enough. Calculate Full Sail Student drop-out rates and let them know the probability of getting a job upon graduation from the BS Web Design & Dev degree program. 
 
-// The program is designed to take 27 months to complete. Of those that completed the program in 2012-2013, 55% finished in 27 months. The job placement rate for students who completed this program is 75%.
-var passed = 0;
-var failed = 0;
+var passed = 0; //Container for number of students passed
+var failed = 0; //Container for number of students failed
 
-var studentList = ["Sarah" , "Bob" , "Carl" , "Daryl" , "Rick" , "Carole" , "Khaleesi" , "Hodor" , "Bethany" , "Charles" , "Beebee" , "Jessica" , "Garen" , "Onyx" , "Dean" , "Sam"];
+var studentList = ["Sarah" , "Bob" , "Carl" , "Daryl" , "Rick" , "Carole" , "Khaleesi" , "Hodor" , "Bethany" , "Charles" , "Beebee" , "Jessica" , "Garen" , "Onyx" , "Dean" , "Sam"]; //This array holds the names of all the students in class this month 
 
-var student = prompt ("Did " + studentList[0] + " graduate?");
-var student2 = prompt ("Did " + studentList[1] + " graduate?");
-var student3 = prompt ("Did " + studentList[2] + " graduate?");
-var student4 = prompt ("Did " + studentList[3] + " graduate?");
-var student5 = prompt ("Did " + studentList[4] + " graduate?");
-var student6 = prompt ("Did " + studentList[5] + " graduate?");
-var student7 = prompt ("Did " + studentList[6] + " graduate?");
-var student8 = prompt ("Did " + studentList[7] + " graduate?");
-var student9 = prompt ("Did " + studentList[8] + " graduate?");
-var student10 = prompt ("Did " + studentList[9] + " graduate?");
-var student11 = prompt ("Did " + studentList[10] + " graduate?");
-var student12 = prompt ("Did " + studentList[11] + " graduate?");
-var student13 = prompt ("Did " + studentList[12] + " graduate?");
-var student14 = prompt ("Did " + studentList[13] + " graduate?");
-var student15 = prompt ("Did " + studentList[14] + " graduate?");
-var student16 = prompt ("Did " + studentList[15] + " graduate?");
+var student = prompt ("Did " + studentList[0] + " graduate?"); //Prompts the user for whether student #1 has passed
+var student2 = prompt ("Did " + studentList[1] + " graduate?"); //Prompts the user for whether student #2 has passed
+var student3 = prompt ("Did " + studentList[2] + " graduate?"); //Prompts the user for whether student #3 has passed
+var student4 = prompt ("Did " + studentList[3] + " graduate?"); //Prompts the user for whether student #4 has passed
+var student5 = prompt ("Did " + studentList[4] + " graduate?"); //Prompts the user for whether student #5 has passed
+var student6 = prompt ("Did " + studentList[5] + " graduate?"); //Prompts the user for whether student #6 has passed
+var student7 = prompt ("Did " + studentList[6] + " graduate?"); //Prompts the user for whether student #7 has passed
+var student8 = prompt ("Did " + studentList[7] + " graduate?"); //Prompts the user for whether student #8 has passed
+var student9 = prompt ("Did " + studentList[8] + " graduate?"); //Prompts the user for whether student #9 has passed
+var student10 = prompt ("Did " + studentList[9] + " graduate?"); //Prompts the user for whether student #10 has passed
+var student11 = prompt ("Did " + studentList[10] + " graduate?"); //Prompts the user for whether student #11 has passed
+var student12 = prompt ("Did " + studentList[11] + " graduate?"); //Prompts the user for whether student #12 has passed
+var student13 = prompt ("Did " + studentList[12] + " graduate?"); //Prompts the user for whether student #13 has passed
+var student14 = prompt ("Did " + studentList[13] + " graduate?"); //Prompts the user for whether student #14 has passed
+var student15 = prompt ("Did " + studentList[14] + " graduate?"); //Prompts the user for whether student #15 has passed
+var student16 = prompt ("Did " + studentList[15] + " graduate?"); //Prompts the user for whether student #16 has passed
 
 
 var studentStatus = student.toUpperCase(); 
