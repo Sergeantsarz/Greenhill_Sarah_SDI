7fa56500d548743b55a941cf09aa97d7df743d7c
Sarah Greenhill <sarah@sarahgreenhill.com>
2014-12-11 20:32:38 -0500

    Adding Comments Personal
    Started adding comments to the personal conditionals assignment.

--- Greenhill_Sarah_ConditionalsAssignment/Conditionals_Personal/js/script.js
+++ Greenhill_Sarah_ConditionalsAssignment/Conditionals_Personal/js/script.js
@@ -2,21 +2,21 @@
 
 //Christmastime means presents and presents mean money. There are 14 different people that need to be purchased for with a total budget of 200 dollars. The grab bag and friends must be $20 each. 
 
-var grabBag = 20;
-var friends = 20;
-var giftees = ["Luke" , "Kristy" , "Joel" , "Gabby" , "Dee" , "Mom", "Dad" , "inlaws" , "Uncle Chuck" , "Jen" , "Auntie Debbie"];
-var giftBudget = prompt ("What would you like to spend, in total?");
-var giftPrice = prompt ("Please enter how much you want to spend on " + giftees[0] + ".");
-var giftPrice2 = prompt ("Please enter how much you want to spend on " + giftees[1] + ".");
-var giftPrice3 = prompt ("Please enter how much you want to spend on " + giftees[2] + ".");
-var giftPrice4 = prompt ("Please enter how much you want to spend on " + giftees[3] + ".");
-var giftPrice5 = prompt ("Please enter how much you want to spend on " + giftees[4] + ".");
-var giftPrice6 = prompt ("Please enter how much you want to spend on " + giftees[5] + ".");
-var giftPrice7 = prompt ("Please enter how much you want to spend on " + giftees[6] + ".");
-var giftPrice8 = prompt ("Please enter how much you want to spend on " + giftees[7] + ".");
-var giftPrice9 = prompt ("Please enter how much you want to spend on " + giftees[8] + ".");
-var giftPrice10 = prompt ("Please enter how much you want to spend on " + giftees[9] + ".");
-var giftPrice11 = prompt ("Please enter how much you want to spend on " + giftees[10] + ".");
+var grabBag = 20;//Place holder to remember these items need to be deducted from the total budget
+var friends = 20;//Place holder to remember these items need to be deducted from the total budget
+var giftees = ["Luke" , "Kristy" , "Joel" , "Gabby" , "Dee" , "Mom", "Dad" , "inlaws" , "Uncle Chuck" , "Jen" , "Auntie Debbie"]; //This array holds the names of the giftees this holiday season
+var giftBudget = prompt ("What would you like to spend, in total?"); //Prompts the user for what they would like to spend in total on all giftees + friends and a grab bag
+var giftPrice = prompt ("Please enter how much you want to spend on " + giftees[0] + "."); //Prompts the user for what they'd like to spend on giftee #1
+var giftPrice2 = prompt ("Please enter how much you want to spend on " + giftees[1] + ".");//Prompts the user for what they'd like to spend on giftee #2
+var giftPrice3 = prompt ("Please enter how much you want to spend on " + giftees[2] + ".");//Prompts the user for what they'd like to spend on giftee #3
+var giftPrice4 = prompt ("Please enter how much you want to spend on " + giftees[3] + ".");//Prompts the user for what they'd like to spend on giftee #4
+var giftPrice5 = prompt ("Please enter how much you want to spend on " + giftees[4] + ".");//Prompts the user for what they'd like to spend on giftee #5
+var giftPrice6 = prompt ("Please enter how much you want to spend on " + giftees[5] + ".");//Prompts the user for what they'd like to spend on giftee #6
+var giftPrice7 = prompt ("Please enter how much you want to spend on " + giftees[6] + ".");//Prompts the user for what they'd like to spend on giftee #7
+var giftPrice8 = prompt ("Please enter how much you want to spend on " + giftees[7] + ".");//Prompts the user for what they'd like to spend on giftee #8
+var giftPrice9 = prompt ("Please enter how much you want to spend on " + giftees[8] + ".");//Prompts the user for what they'd like to spend on giftee #9
+var giftPrice10 = prompt ("Please enter how much you want to spend on " + giftees[9] + ".");//Prompts the user for what they'd like to spend on giftee #10
+var giftPrice11 = prompt ("Please enter how much you want to spend on " + giftees[10] + ".");//Prompts the user for what they'd like to spend on giftee #11
 
 if (giftBudget === ""){
 	
