fac1b8abeb49fdc11cd9acd639f7c3846e356751
Sarah Greenhill <sarah@sarahgreenhill.com>
2014-12-15 16:27:52 -0500

    Functions - Arguments & Parameters
    Learning the difference between arguments and parameters.

--- Functions/js/script.js
+++ Functions/js/script.js
@@ -1,4 +1,4 @@
-//Sarah R. Greenhill - 12/15/14  - Functions - Variable Scope 
+//Sarah R. Greenhill - 12/15/14  - Functions - Arguments & Parameters 
 
 //Functions being "invoked" is being called or summoned into action 
 
@@ -7,14 +7,29 @@
 	
 }*/
 
-var width = 5; //The "scope" of a variable is determined by the location of the variable itself. This variable is scoped to the larger document, whereas the variables inside the calcArea function are scoped to that function specifically. 
+//var width = 5; 
+//The "scope" of a variable is determined by the location of the variable itself. This variable is scoped to the larger document, whereas the variables inside the calcArea function are scoped to that function specifically. 
 
-function calcArea(){
+/* function calcArea(){
 	
 	var width = 20; 
 	var height = 30;
 	var area = width * height;
 	console.log(area);
-} //The actual function definition. What the function "does". 
+} //The actual function definition. What the function "does". */
 
-calcArea(); //This is the function being invoked or "called" 
+//calcArea(30, 20); //This is the function being invoked or "called" with the argmuents or values being input or stored in the parameters below
+
+/*function calcArea(w, h){ //w=30 and h=20 <-arguments and w/h would be the parameter
+	var area = w * h;
+	console.log();
+}*/
+
+function dogYears(age){ //Parameters are what is changed 
+	
+	var dogYears = age * 7;
+	console.log("Sparky is " + dogYears + " years old.");
+}
+
+dogYears(4); //The argument is the value that can be changed
+dogYears(5); //The value is being "passed" in
