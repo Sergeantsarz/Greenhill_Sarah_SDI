dafe37e1f037546af758609292ecca1bfae2cad8
Sarah Greenhill <sarah@sarahgreenhill.com>
2014-12-15 14:17:14 -0500

    Functions - Function Execution
    Learning about functions, what they do, and “invoking” them or calling
    them in order to get what’s inside our function to execute.

--- Functions/js/script.js
+++ Functions/js/script.js
@@ -1,4 +1,6 @@
-//Sarah R. Greenhill - 12/15/14  - Functions - Basic Structure
+//Sarah R. Greenhill - 12/15/14  - Functions - Function Execution
+
+//Functions being "invoked" is being called or summoned into action 
 
 function outptMsg(){
 	console.log("Hello World!");
@@ -11,4 +13,6 @@ function calcArea(){
 	var height = 30;
 	var area = width * height;
 	console.log(area);
-}
+} //The actual function definition. What the function "does". 
+
+calcArea(); //This is the function being invoked or "called" 
