a01d6b77e8fe11593949c67af5c960ab0c753687
Sarah Greenhill <sarah@sarahgreenhill.com>
2014-12-15 16:41:32 -0500

    Functions - Anonymous Functions
    Learning about anonymous functions, how they’re used, why.

--- Functions/js/script.js
+++ Functions/js/script.js
@@ -1,7 +1,8 @@
-//Sarah R. Greenhill - 12/15/14  - Functions - Function vs Procedure 
+//Sarah R. Greenhill - 12/15/14  - Functions - Anonymous Functions 
 
 //Functions being "invoked" is being called or summoned into action 
 //Functions return values, procedures do not
+//Anonymous functions are named and created at the same time it is excecuted, they HAVE to be defined BEFORE they are called. They are used in object-oriented javascript 
 
 /*function outptMsg(){
 	console.log("Hello World!");
@@ -35,10 +36,31 @@
 dogYears(4); //The argument is the value that can be changed
 dogYears(5); //The value is being "passed" in
 */
-var total = calcArea(w, h);//functions that are returning should be assigned variables 
+/*var total = calcArea(w, h);//functions that are returning should be assigned variables 
 
 function calcArea(w, h){
 	var area = w * h;
 	return area; //function is spitting the info out and needs to be stored somewhere 
 	}
 console.log(total);
+*/
+
+/* var functionName = function(){
+	
+	//code to run goes here 
+} */
+
+ /* function calcArea(width, height){
+	var area = width * height;
+	return area; 
+	} */ 
+	// turn this function into an anonymous one 
+	
+	var calcArea = function(width, height) { //defining
+		//code the function runs 
+		var area = width * height;
+		return area;
+	}
+var a = calcArea(20, 30); //invoke
+console.log(a);
+	
