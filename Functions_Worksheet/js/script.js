6da36a8947307e16af58b3060b75ebbd3e55a5f6
Sarah Greenhill <sarah@sarahgreenhill.com>
2014-12-16 16:53:00 -0500

    Functions Worksheet Setup
    Setting up the files for the functions worksheet.

---
+++ Functions_Worksheet/js/script.js
@@ -0,0 +1,5 @@
+//Sarah R. Greenhill - 12/16/14 - Functions Worksheet
+
+//Circumference - Calculate the circumference of a circle. 
+
+//Stung! It takes 8.666666667 bee stings per pound to kill an animal. Calculate how many bee stings are needed to kill an animal in a function 
