27c23fd4fd194076685bfdf370d325a169cf1959
Sarah Greenhill <sarah@sarahgreenhill.com>
2014-12-16 22:43:15 -0500

    Functions Worksheet - Stung!
    Finished Stung! Function.

--- Functions_Worksheet/js/script.js
+++ Functions_Worksheet/js/script.js
@@ -1,10 +1,17 @@
 //Sarah R. Greenhill - 12/16/14 - Functions Worksheet
 
 //Circumference - Calculate the circumference of a circle. Diameter = 2 * radius : Circumference = 3.14 * Diameter : Area = 3.14  * radius^2
-var radius = 4;
-	var d = 2 * radius;
-	var p = 3.14; 
+
+var radius = prompt ("What's the radius of the circle?");
 	
+		if(radius === ""){
+	
+	radius = prompt ("You didn't choose a radius!");
+}
+
+var d = 2 * radius;
+var p = 3.14; 
+
 var circTotal = circumference(d, p);
 
 function circumference(d, p){
@@ -12,7 +19,7 @@ function circumference(d, p){
 	return d * p;
 	
 }
-console.log("The circumference of the circle is " + circTotal + ".");
+console.log("The radius of the circle is " + radius + ". The circumference of the circle is " + circTotal + ".");
 
 
 
@@ -20,4 +27,20 @@ console.log("The circumference of the circle is " + circTotal + ".");
 
 //Stung! It takes 8.666666667 bee stings per pound to kill an animal. Calculate how many bee stings are needed to kill an animal in a function 
 
-console.log("It takes " + + " bee stings to kill this animal.");
+var animalWeight = prompt ("What's the animal's weight?"); 
+
+if(animalWeight === ""){
+	
+	animalWeight = prompt ("You didn't choose a weight!");
+}
+
+
+var beeSting = 8.666666667
+var totalStings = howMany(beeSting, animalWeight);
+
+function howMany(){
+	return animalWeight / beeSting ;
+	
+	
+}
+console.log("Your animal weighs " + animalWeight+ " pound(s). It takes " + totalStings + " bee sting(s) to kill this animal.");
