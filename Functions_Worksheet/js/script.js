1f7a6782b9c6ae8eddf43681eea5fe08df2b6858
Sarah Greenhill <sarah@sarahgreenhill.com>
2014-12-16 22:26:29 -0500

    Functions Worksheet - Circumference
    Finished Circumference Function.

--- Functions_Worksheet/js/script.js
+++ Functions_Worksheet/js/script.js
@@ -1,8 +1,22 @@
 //Sarah R. Greenhill - 12/16/14 - Functions Worksheet
 
 //Circumference - Calculate the circumference of a circle. Diameter = 2 * radius : Circumference = 3.14 * Diameter : Area = 3.14  * radius^2
+var radius = 4;
+	var d = 2 * radius;
+	var p = 3.14; 
+	
+var circTotal = circumference(d, p);
 
-console.log("The circumference of the circle is " + + ".");
+function circumference(d, p){
+	
+	return d * p;
+	
+}
+console.log("The circumference of the circle is " + circTotal + ".");
+
+
+
+
 
 //Stung! It takes 8.666666667 bee stings per pound to kill an animal. Calculate how many bee stings are needed to kill an animal in a function 
 
