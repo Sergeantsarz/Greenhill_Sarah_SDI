170346ef2261a83fb88c0ec6ea8bfdace0769cbb
Sarah Greenhill <sarah@sarahgreenhill.com>
2014-12-16 22:48:24 -0500

    Functions Worksheet - Validation
    Finished the validation for the functions worksheet.

--- Functions_Worksheet/js/script.js
+++ Functions_Worksheet/js/script.js
@@ -7,6 +7,10 @@ var radius = prompt ("What's the radius of the circle?");
 		if(radius === ""){
 	
 	radius = prompt ("You didn't choose a radius!");
+}else if (isNaN(radius)){
+
+	radius = prompt ("Only use numbers for your radius!");
+	
 }
 
 var d = 2 * radius;
@@ -32,6 +36,10 @@ var animalWeight = prompt ("What's the animal's weight?");
 if(animalWeight === ""){
 	
 	animalWeight = prompt ("You didn't choose a weight!");
+}else if (isNaN(animalWeight)){
+
+	animalWeight = prompt ("Only use numbers to select the weight of your animal!");
+	
 }
 
 
