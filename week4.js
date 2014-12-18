b3f55ac28c2311cab86315face6046e259c63632
Sarah Greenhill <sarah@sarahgreenhill.com>
2014-12-18 11:14:58 -0500

    Setup file for week 4 Goto
    Setup the js file

---
+++ week4.js
@@ -0,0 +1,6 @@
+//Sarah R. Greenhill - 12/18/14 - ADI Week 4 GoTo 
+
+//Create a function that spits out a random integer between two given values. 
+//Ask the user for a minimum number
+
+var min = prompt("Let's get a random number between two values. \nPlease type in the minimum value.");
