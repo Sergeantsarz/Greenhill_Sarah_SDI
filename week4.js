c35917a1e655989b441414b254321949565e468e
Sarah Greenhill <sarah@sarahgreenhill.com>
2014-12-18 12:06:02 -0500

    Finished up
    Finished up the different types of validation

--- week4.js
+++ week4.js
@@ -42,10 +42,42 @@ while(isNaN(min)){
 	
 } */
 
+//Ask the user for a max value 
+
+var max =  prompt("Please type in a maximum value!");
+
+//Validate the prompt
+While(max === "" || isNaN(max)){
+	
+	max = prompt("Please do not leave blank and only use numbers. \nPlease type in a max value!");
+}
+
+//Parameters are used to catch the incoming arguments
+function randomizer(minNum , maxNum){
+	//What happens inside the function, stays inside the function
+	console.log("Inside the function");
+	//Use paraeters instead of the main code variables ORDER MATTERS
+	//Find a random number between two values 
+	//How to get random number - Math.random() * (max value - min value) + min value
+	var randomNumber = Math.round(Math.random() * (maxNum - minNum) + Number(minNum));
+	//DO NOT USE CONSOLE.LOG INSIDE OF A FUNCTION - use return values instead
+	
+}
+
+//We need a fucntion call to "run" the function
+//Arguments go inside the function call 
+//What information in our main code, does our function need access to 
+
+//We have to "catch" the returned value of our function call
+var results = randomizer(min , max);
+
+//We use this results variabl in our final console.log IN MAIN CODE
+console.log("The random number is " + results ".");
+
 //Test if the user types in something specific
 //Example yes or no questions
 
-var userGoodMood = prompt("Are you in a good mood? \nPlease answer yes or no.");
+/* var userGoodMood = prompt("Are you in a good mood? \nPlease answer yes or no.");
 
 //Needs && because of the !=
 while(userGoodMood != "yes" && userGoodMood != "Yes" && userGoodMood != "no" && userGoodMood != "No"){
@@ -60,4 +92,4 @@ if(userGoodMood === "yes" || userGoodMood === "Yes"){
 }else{
 	
 	console.log("Sorry to hear that, mabe you should code more. \nThat should make you happy!");
-}
+}*/
