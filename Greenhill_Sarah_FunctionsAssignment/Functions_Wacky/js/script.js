41689bd93273ddb380be97cf114c00a51d52271d
Sarah Greenhill <sarah@sarahgreenhill.com>
2014-12-18 17:41:41 -0500

    Functions - Wacky Setup
    Setting up the wacky function js file.

--- Greenhill_Sarah_FunctionsAssignment/Functions_Wacky/js/script.js
+++ Greenhill_Sarah_FunctionsAssignment/Functions_Wacky/js/script.js
@@ -1,1 +1,6 @@
-//Sarah R. Greenhill - 12/18/14 - Wacky Functions
+//Sarah R. Greenhill - 12/18/14 - Wacky Functions 
+//You took a trip to vegas, enter your story, how many drinks you had, how long it's been since your last drink, and your sex. Get a readout of your story, your blood alcohol the past evening and your current blood alcohol level
+ 
+//Formula - BAC = (Standard Drinks * 0.06 * 100 * 1.055 / Weight * Gender Constant) - (0.015 * Hours)
+//One 12oz beer, 5oz glass of wine or 1.5oz shot of liquor each equal one standard drink
+//Gender constant is 0.68 for men and 0.55 for women. 
