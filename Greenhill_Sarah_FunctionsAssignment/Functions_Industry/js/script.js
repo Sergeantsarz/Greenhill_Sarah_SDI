8b009967fc5a4e1381e33fb8b98c00cd85cb0fbe
Sarah Greenhill <sarah@sarahgreenhill.com>
2014-12-18 23:04:19 -0500

    Function - Industry Setup
    File setup, assigning variables

--- Greenhill_Sarah_FunctionsAssignment/Functions_Industry/js/script.js
+++ Greenhill_Sarah_FunctionsAssignment/Functions_Industry/js/script.js
@@ -1,1 +1,16 @@
-//Sarah R. Greenhill - 12/18/14 - Industry
+//Sarah R. Greenhill - 12/18/14 - Industry Functions
+
+//The user should be able to input popular/well known color names and output the hex number for that color
+
+
+
+var hex1 = "#000000"; //black
+var hex2 = "#ffffff"; //white
+var hex3 = "#ff0000"; //red
+var hex4 = "#cccccc"; //grey
+var hex5 = "#ff00ff"; //magenta
+var hex6 = "#0000ff"; //blue
+var hex7 = "#008000"; //green
+var hex8 = "#800080"; //purple
+var hex9 = "#ffff00"; //yellow
+var hex10 = "#00ffff"; //Cyan
