d66ac808e0939e216268d7df7c7ad2f97b0766ba
Sarah Greenhill <sarah@sarahgreenhill.com>
2014-12-18 20:52:48 -0500

    Functions - Personal
    File set up for the personal function assignment in week 4.

--- Greenhill_Sarah_FunctionsAssignment/Functions_Personal/js/script.js
+++ Greenhill_Sarah_FunctionsAssignment/Functions_Personal/js/script.js
@@ -1,1 +1,4 @@
-//Sarah R. Greenhill - 12/18/14 - Personal Functions
+//Sarah R. Greenhill - 12/18/14 - Personal Functions 
+
+//The user wants to get a new dog. Prompt the user to input their budget, choose a dog breed from a list, whether or not it is purebred and calculate whether or not they can afford the dog. Also calculate how many dogs they can afford based upon their budget. 
+
