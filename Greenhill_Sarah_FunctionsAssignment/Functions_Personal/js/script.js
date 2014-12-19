27a79f5987775170c919351c3d3205b3bd73c794
Sarah Greenhill <sarah@sarahgreenhill.com>
2014-12-18 22:49:26 -0500

    Function - Personal Finished
    Finished the code for week 4 Personal Function, started adding comments
    for each line of code.

--- Greenhill_Sarah_FunctionsAssignment/Functions_Personal/js/script.js
+++ Greenhill_Sarah_FunctionsAssignment/Functions_Personal/js/script.js
@@ -4,21 +4,53 @@
 
 var dogPrice = [2500 , 1450 , 1000 , 1650 , 1250 , 1250 , 2000 , 1500 , 1350 , 2050];
 
-
 var dogCost = 0;
 
 
 var dogType = prompt ("Please choose a dog from this list: \n\n-Poodle, Boxer, German Shepherd, Bichon Frise, Golden Retriever, Laborador, Schnauzer, Bulldog, Pug, or Yorkshire Terrier-");
+
+while(!isNaN(dogType) || dogType === ""){
 	
-var dogBudget =	prompt ("What is your budget for your new dog?");
+if(dogType === ""){
+	dogType = prompt("You didn't enter anything! Which breed of dog do you want?");
+	}else {
+		dogType = prompt("Only use letters please! Which dog breed do you want?");
+	}
+} //Validates for the dogType prompt if they left it blank or used numbers instead of text
+
+while(dogType != "Poodle" && dogType != "poodle" && dogType != "Boxer" && dogType != "boxer" && dogType != "German Shepherd" && dogType != "german shepherd" && dogType != "Bichon Frise" && dogType != "bichon frise" && dogType != "Golden Retriever" && dogType != "Laborador" && dogType != "laborador" && dogType != "Schnauzer" && dogType != "schauzer" && dogType != "Bulldog" && dogType != "bulldog" && dogType != "Pug" && dogType != "pug" && dogType != "Yorkshire Terrier" && dogType != "yorkshire terrier"){
+	
+	dogType = prompt("You didn't choose a dog from the list! \n\nPlease choose a dog from this list: \n\n-Poodle, Boxer, German Shepherd, Bichon Frise, Golden Retriever, Laborador, Schnauzer, Bulldog, Pug, or Yorkshire Terrier-");
+} //Validates to see if the user chose a dog from the list 
+
+var dogBudget =	prompt ("What is your budget for your new dog?"); //Prompts the user for their budget
+
+while(isNaN(dogBudget) || dogBudget === ""){
+	
+if(dogBudget === ""){
+	dogBudget = prompt("You didn't enter anything! What is your budget?");
+	}else {
+		dogBudget = prompt("Only use numbers please! What is your budget?");
+	}
+} //Validates for the dogBudget prompt if they left it blank or used text instead of numbers
+
 
 var pureBred = prompt ("Is the dog you seek a purebred?");
+
+while(!isNaN(pureBred) || pureBred === ""){
+	
+if(pureBred === ""){
+	pureBred = prompt("You didn't enter anything! Do you want a pure bred dog?");
+	}else {
+		pureBred = prompt("Only use letters please! Do you want a pure bred dog?");
+	}
+} //Validates for the pureBred prompt if they left it blank or used text instead of numbers
 
 if(pureBred === "No" || pureBred === "no"){
 	
 	dogCost - 500; 
 	
-}
+} //Deduct from price if the dog is not pure bred 
 
 if(dogType === "Poodle" || dogType === "poodle"){
 		
@@ -62,14 +94,11 @@ if(dogType === "Poodle" || dogType === "poodle"){
 	}
 
 
-
 dogTotal = function() {
  return dogBudget - dogCost;
 }; //Anonymous function containing the left over money after purchase of the dog
  
 var dogAfford = dogTotal();
-
-
 
 console.log("The dog breed you chose is " + dogType + " and your budget is " + dogBudget + ". In order to afford this dog, you needed " + dogCost + ". You have $" + dogAfford + " left over.");
 
